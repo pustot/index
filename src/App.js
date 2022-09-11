bb35a895b26b6c1b0a3a6c1311d66d3c3ef0cd2c
yangcxPusto <619980817@qq.com>
2022-09-11 17:28:43 +0100

    add color mode toggler

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./styles.css";
+// import "./styles.css";
 import Chip from '@mui/material/Chip';
 import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
@@ -13,13 +13,45 @@ import hanpoly from './assets/hanpoly.png'
 import qieyunAutoderiver from './assets/qieyun-autoderiver.png'
 import love from './assets/love.png'
 
-export default function App() {
+import CssBaseline from '@mui/material/CssBaseline';
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
+function App() {
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
 
   const domain = "http://yangcx.tk/";
 
   return (
     <div>
+      
+
       <Container maxWidth="sm">
+      <Stack direction="row" justifyContent="flex-end">
+          <Box
+          sx={{
+            display: 'flex',
+            width: '100%',
+            alignItems: 'center',
+            justifyContent: 'end',
+            bgcolor: 'background.default',
+            color: 'text.primary',
+            borderRadius: 1,
+            p: 3,
+          }}
+          >
+            {theme.palette.mode} mode
+            <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+              {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
+          </Box>
+      </Stack>
       <Stack spacing={2}>
         <Typography  variant="h1">
           Chenxi Yang
@@ -53,5 +85,36 @@ export default function App() {
       </Stack>
       </Container>
     </div>
+  );
+}
+
+export default function AppWithColorToggler() {
+  const [mode, setMode] = React.useState('light');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
